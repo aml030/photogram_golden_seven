de3fae8ee1d6965d992e7eb3ff426cc1b5e4f5ec
aml030 <alorenz2016@kellogg.northwestern.edu>
2015-05-08 12:04:30 -0500

    C in CRUD done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,15 @@ def show
   @photo = Photo.find(params[:id])
 end
 
+def new_form
+end
+
+def create_row
+  p = Photo.new
+  p.source = params["photo_image_url"]
+  p.caption = params["photo_caption"]
+  p.save
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  #to CREATE
+
+get("/photos/new", { :controller => "photos", :action => "new_form" })
+get("/create_photo", { :controller => "photos", :action => "create_row" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
 
 end
